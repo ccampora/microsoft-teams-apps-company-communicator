2006ea281c5a3d351df832f0018e9e11642e9ff8
Cristián Cámpora Guajardo <ccampora@microsoft.com>
2021-04-13 23:16:55 +0200

    Add capacity for user-click tracking.

---
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/ClientApp/build/precache-manifest.a46f89388eea7321a2678509ab9f94ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac585a9972e68aca6055a0ee5f466206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3ede1ae2d0b350f8db6",
+    "url": "/static/css/main.d0471924.chunk.css"
+  },
+  {
+    "revision": "9447e28b19ab69d9ce88",
+    "url": "/static/js/2.438758e4.chunk.js"
+  },
+  {
+    "revision": "4fd523948c1e11a2216af24d455251ac",
+    "url": "/static/js/2.438758e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ede1ae2d0b350f8db6",
+    "url": "/static/js/main.6d6afc51.chunk.js"
+  },
+  {
+    "revision": "a32b5b1f7993dac6c8a5",
+    "url": "/static/js/runtime-main.94f2678d.js"
+  },
+  {
+    "revision": "03c2e8629c48ab3a4791f8354a902dd0",
+    "url": "/static/media/TeamsAssets-Light.03c2e862.eot"
+  },
+  {
+    "revision": "4521ef7e401ab922adbe6019d2c1b015",
+    "url": "/static/media/TeamsAssets-Light.4521ef7e.woff"
+  },
+  {
+    "revision": "5c50f55fa067736aabd6a0248fa193f2",
+    "url": "/static/media/TeamsAssets-Light.5c50f55f.ttf"
+  },
+  {
+    "revision": "9c0b4d34e890dee987096be52b499ce5",
+    "url": "/static/media/TeamsAssets-Light.9c0b4d34.svg"
+  },
+  {
+    "revision": "d7d7eb79c2b2c8d705bd24e50090817b",
+    "url": "/static/media/TeamsAssets-Light.d7d7eb79.woff2"
+  },
+  {
+    "revision": "2de16aabf919bce6480fe5be06fd5928",
+    "url": "/static/media/TeamsAssets-Regular.2de16aab.woff2"
+  },
+  {
+    "revision": "2ed5eaeaa85b5ae4fcb6fa9451934437",
+    "url": "/static/media/TeamsAssets-Regular.2ed5eaea.woff"
+  },
+  {
+    "revision": "76f7e5927a28f5177c391e872afca92d",
+    "url": "/static/media/TeamsAssets-Regular.76f7e592.ttf"
+  },
+  {
+    "revision": "c5a2112b4020146201ccb443a2adfacc",
+    "url": "/static/media/TeamsAssets-Regular.c5a2112b.eot"
+  },
+  {
+    "revision": "de8a9eb155049912556a1e3d9000709a",
+    "url": "/static/media/TeamsAssets-Regular.de8a9eb1.svg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  }
+]);
